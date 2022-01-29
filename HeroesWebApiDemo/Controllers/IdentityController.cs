fb3719fdbbea01ba55be3cc556117dd09de9b8df
Arseniy Golovin <gol.senya.13@gmail.com>
2022-01-29 16:21:44 +0300

    Added UserLogin and UserRegistration dtos Started implementing IdentityController

--- HeroesWebApiDemo/Controllers/IdentityController.cs
+++ HeroesWebApiDemo/Controllers/IdentityController.cs
@@ -1,6 +1,29 @@
-﻿namespace HeroesWebApiDemo.Controllers;
+﻿using HeroesWebApiDemo.Dtos;
+using HeroesWebApiDemo.Services;
+using Microsoft.AspNetCore.Mvc;
 
-public class IdentityController
+namespace HeroesWebApiDemo.Controllers;
+
+[ApiController]
+[Produces("application/json")]
+[Route("[controller]")]
+public class IdentityController : Controller
 {
-    
+    private readonly IIdentityService _identityService;
+
+    public IdentityController(IIdentityService identityService)
+    {
+        _identityService = identityService;
+    }
+
+    [HttpPost("/register")]
+    public async Task<IActionResult> Register([FromBody] UserRegistrationDto userRegistrationDto)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest();
+        }
+
+        throw new NotImplementedException();
+    }
 }
---
+++ HeroesWebApiDemo/Dtos/UserRegistrationDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using JetBrains.Annotations;
+
+namespace HeroesWebApiDemo.Dtos;
+
+[UsedImplicitly(ImplicitUseTargetFlags.Members)]
+public class UserRegistrationDto
+{
+    public string UserName { get; set; }
+    [EmailAddress]
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
