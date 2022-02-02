8a53f43cebdfeaf7b73cd8711dcc17134ecf5fb4
Arseniy Golovin <gol.senya.13@gmail.com>
2022-02-02 15:55:33 +0300

    Added MediatR package Created directory hierarchy for CQRS

---
+++ HeroesWebApiDemo/Installers/MediatRInstaller.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace HeroesWebApiDemo.Installers;
+
+public class MediatRInstaller : IInstaller
+{
+    public void InstallServices(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddMediatR(typeof(Program));
+    }
+}
