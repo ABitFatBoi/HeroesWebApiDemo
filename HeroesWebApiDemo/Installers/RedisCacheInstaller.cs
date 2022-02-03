963655b22ed882bad826335c443a654a8af990c3
Arseniy Golovin <gol.senya.13@gmail.com>
2022-02-03 15:19:06 +0300

    Setting up redis cache: added StackExchangeRedis nuGet with corresponding installer and options class

---
+++ HeroesWebApiDemo/Installers/RedisCacheInstaller.cs
@@ -0,0 +1,16 @@
+﻿using HeroesWebApiDemo.Options;
+
+namespace HeroesWebApiDemo.Installers;
+
+public class RedisCacheInstaller : IInstaller
+{
+    public void InstallServices(IServiceCollection services, IConfiguration configuration)
+    {
+        var options = new RedisOptions();
+        configuration.GetSection(nameof(RedisOptions)).Bind(options);
+        
+        if (!options.Enabled) return;
+
+        services.AddStackExchangeRedisCache(opt => opt.Configuration = options.ConnectionString);
+    }
+}
---
+++ HeroesWebApiDemo/Options/RedisOptions.cs
@@ -0,0 +1,11 @@
+﻿using JetBrains.Annotations;
+
+namespace HeroesWebApiDemo.Options;
+
+public class RedisOptions
+{
+    [UsedImplicitly]
+    public bool Enabled { get; set; }
+    [UsedImplicitly]
+    public string ConnectionString { get; set; } = default!;
+}
