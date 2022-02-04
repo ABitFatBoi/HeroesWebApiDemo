a359d1915727a9322f8b4d27ba0598a21850a4f0
Arseniy Golovin <gol.senya.13@gmail.com>
2022-02-04 19:52:49 +0300

    Fixed context synchronization issue in IntegrationTests project. Added GetAll_WithoutAnyHeroes_ReturnsEmptyResponse test

--- HeroesWebApiDemo.IntegrationTests/HeroesControllerTests.cs
+++ HeroesWebApiDemo.IntegrationTests/HeroesControllerTests.cs
@@ -1,44 +1,67 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Text.Json;
 using System.Threading.Tasks;
 using FluentAssertions;
 using HeroesWebApiDemo.Dtos.V1.Requests;
 using HeroesWebApiDemo.Dtos.V1.Responses;
 using HeroesWebApiDemo.Entities;
 using HeroesWebApiDemo.Enums;
+using HeroesWebApiDemo.Migrations;
 using HeroesWebApiDemo.Routes.V1;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
 using Xunit;
 
 namespace HeroesWebApiDemo.IntegrationTests;
 
-public class HeroesControllerTests : IClassFixture<CustomWebApplicationFactory<Program>>
+public class HeroesControllerTests : IClassFixture<WebApplicationFactory<Program>>
 {
     private readonly HttpClient _client;
 
-    public HeroesControllerTests(CustomWebApplicationFactory<Program> factory)
+    public HeroesControllerTests(WebApplicationFactory<Program> factory)
     {
-        _client = factory.CreateClient(new WebApplicationFactoryClientOptions
-        {
-            AllowAutoRedirect = false
-        });
+        _client = factory.WithWebHostBuilder(
+            builder => builder.ConfigureServices(services =>
+            {
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType ==
+                         typeof(DbContextOptions<AppDbContext>));
+
+                if (descriptor != null) services.Remove(descriptor);
+
+                services.AddDbContext<AppDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("inMem");
+                });
+
+                var sp = services.BuildServiceProvider();
+
+                using var scope = sp.CreateScope();
+
+                var scopedServices = scope.ServiceProvider;
+                var db = scopedServices.GetRequiredService<AppDbContext>();
+
+                db.Database.EnsureDeleted();
+            })).CreateClient();
     }
 
-    // [Fact]
-    // public async Task GetAll_WithoutAnyHeroes_ReturnsEmptyResponse()
-    // {
-    //     //Arrange
-    //     await _client.AuthenticateAsync();
-    //     
-    //     //Act
-    //     var response = await _client.GetAsync(ApiRoutes.Heroes.GetAll);
-// 
-    //     //Assert
-    //     response.StatusCode.Should().Be(HttpStatusCode.OK);
-    //     (await JsonSerializer.DeserializeAsync<List<Hero>>(await response.Content.ReadAsStreamAsync())).Should().BeEmpty();
-    // }
+    [Fact]
+    public async Task GetAll_WithoutAnyHeroes_ReturnsEmptyResponse()
+    {
+        //Arrange
+        await _client.AuthenticateAsync();
+        
+        //Act
+        var response = await _client.GetAsync(ApiRoutes.Heroes.GetAll);
+        
+        //Assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        JsonConvert.DeserializeObject<List<Hero>>(await response.Content.ReadAsStringAsync()).Should().BeEmpty();
+    }
 
     [Fact]
     public async Task GetAll_WithTestHeroes_ReturnsAllHeroes()
@@ -64,10 +87,15 @@ public class HeroesControllerTests : IClassFixture<CustomWebApplicationFactory<P
         //Assert
         response.StatusCode.Should().Be(HttpStatusCode.OK);
 
-        var returnedHeroes = new List<HeroResponseDto> { hero1, hero2 };
-        
-        var hero1Returned = returnedHeroes.Find(h => h.Name == "Hero 1 (Ranged Mage)");
-        var hero2Returned = returnedHeroes.Find(h => h.Name == "Hero 2 (Ranged Healer)");
+        var returnedHeroes = JsonConvert.DeserializeObject<List<HeroResponseDto>>(
+            await response.Content.ReadAsStringAsync());
+
+        returnedHeroes.Should().NotBeNull();
+
+        var hero1Returned = returnedHeroes.Find(h => 
+            h.Name == "Hero 1 (Ranged Mage)");
+        var hero2Returned = returnedHeroes.Find(h => 
+            h.Name == "Hero 2 (Ranged Healer)");
 
         hero1Returned.Should().NotBeNull("Can't find hero with the Name \"Hero 1 (Ranged Mage)\"");
         hero2Returned.Should().NotBeNull("Can't find hero with the Name \"Hero 2 (Ranged Healer)\"");
